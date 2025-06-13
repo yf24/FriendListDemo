be69398aef5f14a27390fb9b5d2a45839180c7d9
ming <coopldh@gmail.com>
2025-06-13 18:20:05 +0800

    init

---
+++ FriendListDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendListDemo
+//
+//  Created by Ming on 2025/6/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FriendListDemoTests/FriendListDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListDemoTests.swift
+//  FriendListDemoTests
+//
+//  Created by Ming on 2025/6/13.
+//
+
+import Testing
+@testable import FriendListDemo
+
+struct FriendListDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
