2a76c359b5d2feb72400a2b4613d8b921557c95e
ming <coopldh@gmail.com>
2025-06-21 00:01:11 +0800

    [Impl.] header view (UI)
    xib 先拉好

--- FriendListDemo/Views/Friend/Header/HeaderView.swift
+++ FriendListDemo/Views/Friend/Header/HeaderView.swift
@@ -1,19 +1,31 @@
 import UIKit
 
 class HeaderView: UIView {
+    // nav button
     @IBOutlet weak var atmButton: UIButton!
     @IBOutlet weak var transferButton: UIButton!
     @IBOutlet weak var scanButton: UIButton!
-
-    @IBOutlet weak var avatarImageView: UIImageView!
+    // user info
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var kokoIdLabel: UILabel!
+    @IBOutlet weak var avatarButton: UIButton!
+    // control panel
+    @IBOutlet weak var friendButton: UIButton!
+    @IBOutlet weak var chatButton: UIButton!
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadFromNibToSelf()
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadFromNibToSelf()
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // 預設 UI 設定
-//        avatarImageView.layer.cornerRadius = 28
-//        avatarImageView.clipsToBounds = true
+
 //        nameLabel.text = "紫琳"
 //        kokoIdLabel.text = "KOKO ID：olylinhuang"
     }
